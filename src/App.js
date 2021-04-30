1652ba246a3663c7eb897e454b97fe700fe06d30
agarcluq <agarcluq@everis.com>
2021-04-30 10:03:12 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "./style.css";
 
 export default function App() {
+  // Ejemplo con datos estáticos
+// const datos = [
+//   {id:1,nombre:'React'},
+//   {id:2,nombre:'AngularJs'},
+//   {id:3,nombre:'Vue'},
+// ]
+const [equipo,setEquipo] = useState([])
+// Ejemplo con datos estáticos
+  // useEffect(()=>{
+  //   console.log('hola')
+  //    setEquipo(datos)
+  // },[])
+
+  // Dinámicos
+    useEffect(()=>{
+    console.log('hola')
+
+  },[])
+
+  const obtenerDatos = async () =>{
+    await fetch('https://jsonplaceholder.typicode.com/users')
+  }
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div>{equipo.map((item,index)=>{
+        <p key={index}>{item.equipo}</p>
+      })}</div>
     </div>
   );
 }
